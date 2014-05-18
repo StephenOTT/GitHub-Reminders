3aa9ac46295e3a5d0aa1baae7161ffa4fab496e7
Stephen Russett <stephenrussett@gmail.com>
2014-05-18 14:28:47 -0400

    Added multiple methods for future development

--- app/sinatra_helpers.rb
+++ app/sinatra_helpers.rb
@@ -18,6 +18,10 @@ module Sinatra_Helpers
 
 		# end
 
+		# TODO Add support to mark a hook creation in MongoDB as a "public"
+		# hook which can be registered by other users.  Should also support
+		# private hooks which means the hook creator must manage which users
+		# are allowed to register for that hook.
 		def self.create_gh_hook(fullNameRepo, githubObject)
 			begin
 				githubObject.create_hook(
@@ -38,10 +42,67 @@ module Sinatra_Helpers
 			end
 		end
 
+		# checks if the remidner hook already exsists for a specific repo
+		# should error handle if the user does not have permission to 
+		# read hooks on the repo
+		def self.reminder_hook_exists?(repo)
 
+		end
 
+		# Deletes the reminder hook from a repo.  Only deletes the hook 
+		# if it already exists.  Should be calling the 'reminder_hook_exists?' 
+		# method to determine if the hook exsists and can be deleted.
+		def self.delete_reminder_hook(repo)
 
+		end
 
+		# Creates a new record/profile for the user in MongoDB
+		def self.create_user
+
+		end
+
+		# Checks MongoDB to see if the user has a record/profile
+		def self.user_exists?
+
+		end
+
+		# Updates the user profile in MongoDB (Name, Email, Timezone)
+		def self.update_user_profile
+
+		end
+
+		# uses the GitHub API to get the list of authenticated emails attached to 
+		# the github profile these are the email addreses that are avaliable 
+		# to the user to send reminders to.
+		def self.get_authenticated_github_emails
+
+		end
+
+		# produces the list of timezones to be selected by the user to indicate 
+		# what timezone they live in/posted the github comment from.
+		def self.avalaible_timezones
+
+		end
+
+		# Calculates the number of seconds between Issue Comments Created_At time
+		# and the Reminder Time.  Takes into account the users Timezone, and the
+		# GitHub UTC time.
+		def self.calculate_seconds_between_commentTime_and_reminderTime
+
+		end
+
+		# Lists the repos that have been registered.
+		# Registration of a repo, may not inlvove the creation of the hook.
+		# Hook may have already been created by another user.
+		def self.registered_repos_for_user(userID)
+
+		end
+
+		# Unregisters a user from a created hook. Does not delete the hook
+		# as the hook may be being used by other users
+		def self.un_register_repo_for_user(userID, repo)
+
+		end
 
 
 

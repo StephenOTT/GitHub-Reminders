9e24f515ea633b5d68c2e2774c9ec08bf6a32455
Stephen Russett <stephenrussett@gmail.com>
2014-05-19 01:10:58 -0400

    Added adjustments and additional mongos

--- app/mongo.rb
+++ app/mongo.rb
@@ -5,17 +5,25 @@ module Mongo_Connection
 	include Mongo
 
 	def self.clear_mongo_collections
-		@collReminders.remove
+		@collRemindersUsers.remove
 	end
 
-	def self.addReminder(mongoPayload)
-		@collReminders.insert(mongoPayload)
+	def self.addIntoMongo(mongoPayload)
+		@collRemindersUsers.insert(mongoPayload)
+	end
+
+	def self.find_and_modify_document(options)
+		@collRemindersUsers.find_and_modify(options)
 	end
 
 	def self.mongo_Connect(url, port, dbName, collName)
 		@client = MongoClient.new(url, port)
 		@db = @client[dbName]
-		@collReminders = @db[collName]
+		@collRemindersUsers = @db[collName]
+	end
+
+	def self.aggregate(input)
+		@collRemindersUsers.aggregate(input)
 	end
 
 

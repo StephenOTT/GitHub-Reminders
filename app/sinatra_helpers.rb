9039607ea0ad3814fb1770ae74452ff6748767c7
Stephen Russett <stephenrussett@gmail.com>
2014-05-19 01:11:10 -0400

    Additional helpers for further dev

--- app/sinatra_helpers.rb
+++ app/sinatra_helpers.rb
@@ -1,7 +1,10 @@
 # require_relative '../time_analyzer/system_wide_processor'
 # require 'qless'
-# require_relative 'mongo'
+require_relative 'mongo'
 # require 'octokit'
+# require 'active_support'
+require 'active_support/core_ext/time/zones'
+require 'time_difference'
 
 module Sinatra_Helpers
 
@@ -22,9 +25,9 @@ module Sinatra_Helpers
 		# hook which can be registered by other users.  Should also support
 		# private hooks which means the hook creator must manage which users
 		# are allowed to register for that hook.
-		def self.create_gh_hook(fullNameRepo, githubObject)
+		def self.create_gh_hook(fullNameRepo, githubAPIObject)
 			begin
-				githubObject.create_hook(
+				githubAPIObject.create_hook(
 					fullNameRepo,	'web',
 					{
 						:url => 'http://www.GitHub-Reminders.com/webhook',
@@ -45,65 +48,205 @@ module Sinatra_Helpers
 		# checks if the remidner hook already exsists for a specific repo
 		# should error handle if the user does not have permission to 
 		# read hooks on the repo
-		def self.reminder_hook_exists?(repo)
+		def self.reminder_hook_exists?(repo, githubAPIObject)
+
+			hooks = githubAPIObject.hooks(repo) || []
+			
+			hooks.each do |x|
+				if x.attrs["url"] == "http://www.GitHub-Reminders.com/webhook"
+					return true
+					break
+				else 
+					false
+				end
+			end
 
 		end
 
 		# Deletes the reminder hook from a repo.  Only deletes the hook 
 		# if it already exists.  Should be calling the 'reminder_hook_exists?' 
 		# method to determine if the hook exsists and can be deleted.
-		def self.delete_reminder_hook(repo)
+		def self.delete_reminder_hook(repo, hookID)
+
+		end
+
+		# Determine the hook ID of the Remidner hook
+		def self.determine_reminder_hook_id(repo)
 
 		end
 
 		# Creates a new record/profile for the user in MongoDB
-		def self.create_user
+		def self.create_user(userid, attributes = {})
+			self.mongo_connection
 
+			username 	= attributes[:username]
+			userID		= attributes[:userid]
+			firstName 	= attributes[:firstname] || nil
+			lastName 	= attributes[:lastname] || nil
+			repos 		= []
+
+			userExists = self.user_exists?(userid)
+			if userExists == false
+
+				userData = { :username => username,
+							 :userid => userID,
+							 :firstname => firstName,
+							 :lastname => lastName,
+							 :created_at => Time.now.utc,
+							 :updated_at => Time.now.utc,
+							 :repos => repos
+							}
+
+				self.add_mongo_data(userData)
+
+				return "Record created."
+			elsif userExists == true
+				return "Cannot create record, user already exists."
+			end
 		end
 
-		# Checks MongoDB to see if the user has a record/profile
-		def self.user_exists?
-
+		# Checks MongoDB to see if the user has a record/profile based on the userid
+		def self.user_exists?(userid)
+			users = self.aggregate([
+									{ "$match" => {userid: userid}}
+									])
+			if users.count >= 1
+				return true
+			elsif users.count == 0
+				return false
+			end
 		end
 
 		# Updates the user profile in MongoDB (Name, Email, Timezone)
-		def self.update_user_profile
+		def self.update_user_profile(userID, attributes = {})
+			firstName 	= attributes[:firstname]
+			lastName 	= attributes[:lastname]
+			timezone 	= attributes[:timezone] || nil
+			
+			userData = { :firstname => firstName,
+						 :lastname => lastName,
+						 :updated_at => Time.now.utc
+						}
 
+			self.find_and_modify_document(:query => {"userid" => userID},
+							  				:update => {"$set" => userData}
+											)
 		end
 
 		# uses the GitHub API to get the list of authenticated emails attached to 
 		# the github profile these are the email addreses that are avaliable 
 		# to the user to send reminders to.
-		def self.get_authenticated_github_emails
+		def self.get_authenticated_github_emails(githubAPIObject)
+			emails = githubAPIObject.emails
+			verifiedEmails = []
 
+			emails.each do |e|
+				if e.attrs["verified"] == true
+					verifiedEmails << e.attrs["email"]
+				end
+			end
+
+			return verifiedEmails
 		end
 
-		# produces the list of timezones to be selected by the user to indicate 
-		# what timezone they live in/posted the github comment from.
-		def self.avalaible_timezones
+		# returns true if there are 1 or more verified emails, returns false 
+		# if there are 0 verified emails.  Method is supposed to work with 
+		# the get_authenticated_github_emails emthod
+		def self.verified_emails_exist?(verifiedEmails)
+			if verifiedEmails.count == 0 
+				return false
+			elsif verifiedEmails >= 1
+				return true
+			end
+		end
 
+		# Produces the list of timezones to be selected by the user to indicate 
+		# what timezone they live in/posted the github comment from.
+		# Uses ActiceSupport::TimeZone.all to return list of timezones.
+		def self.avalaible_timezones
+			ActiveSupport::TimeZone.all
 		end
 
 		# Calculates the number of seconds between Issue Comments Created_At time
 		# and the Reminder Time.  Takes into account the users Timezone, and the
 		# GitHub UTC time.
-		def self.calculate_seconds_between_commentTime_and_reminderTime
-
+		def self.calculate_seconds_between_commentTime_and_reminderTime(commentCreatedDate, scheduledDate)
+			TimeDifference.between(commentCreatedDate, scheduledDate).in_seconds
 		end
 
 		# Lists the repos that have been registered.
 		# Registration of a repo, may not inlvove the creation of the hook.
 		# Hook may have already been created by another user.
 		def self.registered_repos_for_user(userID)
+			repos = self.aggregate([
+									{ "$match" => {userid: userid}},
+									{ "$unwind" => "$repos"},
+									# { "$match" => {"repos.repo" => repo}}
+									])
+			if repos == nil 
+				return []
+			end
 
+		end
+
+		# Registers a repo for a specific user
+		def self.register_repo_for_user(userid, repoAttributes = {})
+			repoFullName = repoAttributes[:fullreponame] # example: StephenOTT/Test1
+			created_at = Time.now.utc
+			active = true
+
+			registeredRepoInfo = {:repo => repoFullName,
+								  :created_at => created_at,
+								  :active => active
+								}
+
+			repoRegistered = self.repo_registered?(userid, repoFullName)
+
+			if repoRegistered == false				
+				self.find_and_modify_document(:query => {"userid" => userid},
+											  :update => {"$push" => {"repos" => registeredRepoInfo }}
+											)
+				return "Repo has been successfully registered"
+
+			elsif repoRegistered == true
+				return "Repo is already registered"
+			end
+		end
+
+		# Returns true if the repo is already registered under the specific users
+		def self.repo_registered?(userid, repo)
+			# TODO future rebuild as a non-aggregation call.  Will look into using Find()
+			repos = self.aggregate([
+									{ "$match" => {userid: userid}},
+									{ "$unwind" => "$repos"},
+									{ "$match" => {"repos.repo" => repo}}
+									]).count
+			if repos >= 1
+				return true
+			elsif repos == 0
+				return false
+			end
 		end
 
 		# Unregisters a user from a created hook. Does not delete the hook
 		# as the hook may be being used by other users
-		def self.un_register_repo_for_user(userID, repo)
-
+		def self.un_register_repo_for_user(userid, repo)
+			begin
+				self.find_and_modify_document(:query => { "userid" => userid},
+												:update => {"$pull" => {'repos' => {'repo'=>repo}}}
+											)
+				return "repo has been removed"
+			rescue
+				return "something went wrong when we tried to remove the repo"
+			end
 		end
 
+		# sets the user timezone in MongoDB
+		def self.set_user_timezone(userID, timezone)
+			self.find_and_modify_document(:query => {"userid" => userID},
+										  :update => {"$set" => {"timezone" => timezone.to_s}}
+											)
+		end
 
 
 		# def self.download_time_tracking_data(user, repo, githubObject, githubAuthInfo)
@@ -116,17 +259,34 @@ module Sinatra_Helpers
 		# end
 
 
-		def mongo_connection(clearCollections = true)
-
-			Mongo_Connection.mongo_Connect("localhost", 27017, "GitHub-Analytics", "Issues-Data")
+		def self.mongo_connection(clearCollections = false)
+			Mongo_Connection.mongo_Connect("localhost", 27017, "GitHub-Reminders", "Users")
 
 			if clearCollections == true
 			Mongo_Connection.clear_mongo_collections
 			end
-
 		end
 
-		def add_mongo_data(data)
-			Mongo_Connection.addReminder(data)
+		def self.add_mongo_data(data)
+			Mongo_Connection.addIntoMongo(data)
+		end
+
+		def self.find_and_modify_document(options)
+			Mongo_Connection.find_and_modify_document(options)
+		end
+
+		def self.aggregate(input)
+			Mongo_Connection.aggregate(input)
 		end
 end
+
+# Debug Code
+# Sinatra_Helpers.mongo_connection
+# puts Sinatra_Helpers.user_exists?(1994838)
+# Sinatra_Helpers.set_user_timezone(1994838)
+# Sinatra_Helpers.register_repo_for_user(1994838, {:fullreponame => "stephenott/test1"})
+# puts Sinatra_Helpers.repo_registered?(1994838, "stephenott/test1")
+# puts Sinatra_Helpers.un_register_repo_for_user(1994838, "stephenott/test1")
+# puts Sinatra_Helpers.avalaible_timezones
+# puts Sinatra_Helpers.calculate_seconds_between_commentTime_and_reminderTime(Time.now.utc, Time.now.utc + 5.minutes)
+

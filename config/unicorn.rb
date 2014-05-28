8f0a644f7ff38079b8b158370f64dcf12bd2cc57
Stephen Russett <stephenrussett@gmail.com>
2014-05-28 02:24:44 -0400

    adjustments for unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,3 +8,24 @@ end
 
 timeout 30
 preload_app true
+
+
+before_fork do |server, worker|
+  # ...
+
+  # If you are using Redis but not Resque, change this
+  if defined?(Resque)
+    Qless.redis.quit
+    # Rails.logger.info('Disconnected from Redis')
+  end
+end
+
+after_fork do |server, worker|
+  # ...
+
+  # If you are using Redis but not Resque, change this
+  if defined?(Resque)
+    Qless.redis = ENV['REDIS_URI']
+    # Rails.logger.info('Connected to Redis')
+  end
+end

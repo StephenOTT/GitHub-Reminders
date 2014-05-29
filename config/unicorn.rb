b1582ef16de807a1ca40d06a8989b6176c419497
Stephen Russett <stephenrussett@gmail.com>
2014-05-28 21:20:28 -0400

    adjustement to unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,24 +1,3 @@
-
-# Minimal Heroku unicorn config
-# Heroku has a 30s timeout so we do 60s so we're not double-killing
-# Depending on memory usage workers can be bumped to 3-4
-# preload_app=true can cause issues if you don't reconnect things correctly
-
-preload_app false
-
-worker_processes Integer(ENV['UNICORN_WORKERS'] || 2)
-timeout Integer(ENV['UNICORN_TIMEOUT'] || 25)
-# listen ENV['PORT'], :backlog => Integer(ENV['UNICORN_BACKLOG'] || 200)
-
-before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
-end
-
-after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
-  end
-end
+worker_processes 3
+timeout 30
+preload_app true

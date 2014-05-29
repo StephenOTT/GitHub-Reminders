6ca2dfcfbf44a0900fb06f82cf68a91930264b2f
Stephen Russett <stephenrussett@gmail.com>
2014-05-28 21:55:57 -0400

    qless adjustements

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 
 
 web: bundle exec unicorn -p $PORT -c ./config/unicorn.rb
+worker:  bundle exec rake qless:work
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,6 +3,6 @@ worker_processes 3
 timeout 30
 
 
-before_fork do |server, worker|
-  spawn("bundle exec rake " + "qless:work")
-end
+# before_fork do |server, worker|
+#   spawn("bundle exec rake " + "qless:work")
+# end

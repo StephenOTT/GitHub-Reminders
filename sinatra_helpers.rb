5163c243c3da54fc5580c801733b5b555e678406
Stephen Russett <stephenrussett@gmail.com>
2014-06-19 00:10:33 -0400

    testing

--- sinatra_helpers.rb
+++ sinatra_helpers.rb
@@ -604,11 +604,11 @@ module Sinatra_Helpers
 			emailJobs = []
 			userJobs.each do |x|
 				job = client.jobs[x]
-				# emailJobs << job.klass.to_s
+				emailJobs << job.klass
 			# 	emailJob = client.jobs[x]
-				if job.klass == "SendEmail"
-					emailJobs << job.data
-				end
+				# if job.klass == "SendEmail"
+				# 	emailJobs << job.data
+				# end
 			end
 			return emailJobs
 

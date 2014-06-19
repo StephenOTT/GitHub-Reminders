992539f73350adb89e65bdfd126c7382dcf5ceda
Stephen Russett <stephenrussett@gmail.com>
2014-06-18 21:30:47 -0400

    removed old email code

--- qless/qless_sampler.rb
+++ qless/qless_sampler.rb
@@ -15,12 +15,7 @@ class MyJobClass
   def self.perform(job)
     # job is an instance of `Qless::Job` and provides access to
     # job.data, a means to cancel the job (job.cancel), and more.
-		RestClient.post "https://api:key-6iqs3vfdn7pnkgxpj4ip4-1iyve-ljm3"\
-		"@api.mailgun.net/v2/sandbox7a90f2af1ae6406bbd6f4ef9cff652b3.mailgun.org/messages",
-		"from" => "GitHub Reminder <postmaster@sandbox7a90f2af1ae6406bbd6f4ef9cff652b3.mailgun.org>",
-		"to" => "Stephen Russett <stephenrussett@gmail.com>",
-		"subject" => "GitHub Reminder - 1",
-		"text" => "This is a Github-Reminder"
+
   end
 end
 
